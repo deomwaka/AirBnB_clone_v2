45f06ac64480ec054f4740fe4d5bbb2bf4397804
deomwaka <deogratiusmwaka@gmail.com>
2023-10-11 06:49:45 -0700

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['3.238.180.65', '3.235.62.78']
+env.hosts = ['100.25.165.156', '100.25.205.35']
 
 
 def do_pack():
